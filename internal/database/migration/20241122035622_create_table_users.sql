b54aed58e438daf898223389f51b58512c44c5d7
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-11-22 16:35:16 +0700

    add user seeds

--- internal/database/migration/20241122035622_create_table_users.sql
+++ internal/database/migration/20241122035622_create_table_users.sql
@@ -6,6 +6,19 @@ CREATE TABLE users (
     password_hash TEXT NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
-
+-- Seed data
+INSERT INTO users (id, username, email, password_hash)
+VALUES (
+        '00000000-0000-0000-0000-000000000000',
+        'username1',
+        'username1@example.com',
+        ' $2a$10$fRypLiLtPOrgB2Mvy6Fx5.oZuNwock3V3cVMTAj1wyn.Paam2Oeyu '
+    ),
+    (
+        '00000000-0000-0000-0000-000000000001',
+        'username2',
+        'username2@example.com',
+        ' $2a$10$fRypLiLtPOrgB2Mvy6Fx5.oZuNwock3V3cVMTAj1wyn.Paam2Oeyu '
+    );
 -- +goose Down
 DROP TABLE users;
