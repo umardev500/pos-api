8645d5dad8f0b0c1216c73e587b985169a1b8987
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-11-28 06:44:49 +0700

    add units migration

--- internal/database/migration/20241127234003_create_units_table.sql
+++ internal/database/migration/20241127234003_create_units_table.sql
@@ -6,10 +6,9 @@ CREATE TABLE units (
     multiplier FLOAT NOT NULL
 );
 -- Seed data
-INSERT
 INSERT INTO units (id, "name", multiplier)
-VALUES (uuid_generate_v4(), 'Piece', 1),
-    (uuid_generate_v4(), 'Kilogram', 1000),
-    (uuid_generate_v4(), 'Gram', 1);
+VALUES ("00000000-0000-0000-0000-000000000000", 'Piece', 1),
+    ("00000000-0000-0000-0000-000000000001", 'Kilogram', 1000),
+    ("00000000-0000-0000-0000-000000000002", 'Gram', 1);
 -- +goose Down
 DROP TABLE units;
