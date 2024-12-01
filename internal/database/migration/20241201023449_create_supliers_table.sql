d2b882b444029342c93f5024f9a959d852f248e3
Umar Schweinsteiger <umar.dev500@gmail.com>
2024-12-01 14:40:43 +0700

    add suppliers and stock adjusments migrations

---
+++ internal/database/migration/20241201023449_create_supliers_table.sql
@@ -0,0 +1,15 @@
+-- +goose U
+CREATE TABLE IF NOT EXISTS suppliers (
+    id UUID PRIMARY KEY,
+    "name" VARCHAR(255) NOT NULL UNIQUE,
+    "description" TEXT NOT NULL,
+    "address" VARCHAR(255) NULL,
+    "email" VARCHAR(50) NULL,
+    "phone" VARCHAR(20) NULL,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP NULL DEFAULT NULL,
+    deleted_at TIMESTAMP NULL DEFAULT NULL
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS suppliers;
---
+++ internal/database/migration/20241201023928_create_stock_adjustments_table.sql
@@ -0,0 +1,21 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS stock_adjustments (
+    id UUID PRIMARY KEY,
+    product_id UUID NOT NULL,
+    supplier_id UUID NULL, -- Nullable for non-supplier adjustments
+    quantity INT NOT NULL,
+    reason VARCHAR(255) NOT NULL, -- Reason for the adjustment
+    reversed BOOLEAN NOT NULL DEFAULT FALSE,
+    reversal_of UUID NULL, -- Link to the reversal adjustment
+    created_by UUID NOT NULL, -- User who created the adjustment
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    
+    FOREIGN KEY (product_id) REFERENCES products (id) ON DELETE CASCADE,
+    FOREIGN KEY (supplier_id) REFERENCES suppliers (id) ON DELETE CASCADE,
+    FOREIGN KEY (created_by) REFERENCES users (id) ON DELETE CASCADE,
+    -- Reversal
+    FOREIGN KEY (reversal_of) REFERENCES stock_adjustments (id) ON DELETE CASCADE
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS stock_adjustments;
